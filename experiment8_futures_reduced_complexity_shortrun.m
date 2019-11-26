1ff4d1a623a514686f951f019edde8c604af2311
Lawrence Middleton <lawrencemiddleton1@gmail.com>
2019-11-26 16:09:26 +0000

    Update experiment8_futures_reduced_complexity_shortrun.m

--- experiment8_futures_reduced_complexity_shortrun.m
+++ experiment8_futures_reduced_complexity_shortrun.m
@@ -2,7 +2,7 @@
 % the following aims to sample from the posterior distribution over
 % changepoint locations.
 clear
-rng(2)
+rng(1)
 addpath(genpath('util'))
 addpath(genpath('csmc_cp'));
 addpath(genpath('smc_cp'));
@@ -26,74 +26,76 @@ distribution_names = {'ghyp','sstd'};
 
 dist_indx_arr = [1,1,2,2];
 GDPPrior_arr = [false,true,false,true];
+exp_indx_arr = [1,3];
 
-exp_indx=1;
+for run_exp_indx=1:1
+    exp_indx = exp_indx_arr(run_exp_indx);
 
-dist_indx = dist_indx_arr(exp_indx);
-GDPPrior = GDPPrior_arr(exp_indx);
+    dist_indx = dist_indx_arr(exp_indx);
+    GDPPrior = GDPPrior_arr(exp_indx);
 
-% iterate through each of the distributions
-distribution_name = distribution_names{dist_indx};
-data_fname = sprintf('data/yahoo_data/largestocks2007%s.mat',distribution_name);
-X_mat = load(data_fname);
-X_mat = X_mat.X_mat;
-X = X_mat';
-[n,T] = size(X);
-X = X(:,ceil(T*0.5):end);
-[n,T] = size(X);
+    % iterate through each of the distributions
+    distribution_name = distribution_names{dist_indx};
+    data_fname = sprintf('data/yahoo_data/largestocks2007%s.mat',distribution_name);
+    X_mat = load(data_fname);
+    X_mat = X_mat.X_mat;
+    X = X_mat';
+    [n,T] = size(X);
+    X = X(:,ceil(T*0.5):end);
+    [n,T] = size(X);
 
-z0 = randn(1,T);
-pGeo0 = 0.1;
-tau = [0];
+    z0 = randn(1,T);
+    pGeo0 = 0.1;
+    tau = [0];
 
-% save settings and data to struct
-params.GDPPrior = GDPPrior;
-params.n_quadrature_points = n_quadrature_points;
-params.constrain_rho = constrain_rho;
-params.X = X;
-params.z = z0;
-params.pGeo = pGeo0;
+    % save settings and data to struct
+    params.GDPPrior = GDPPrior;
+    params.n_quadrature_points = n_quadrature_points;
+    params.constrain_rho = constrain_rho;
+    params.X = X;
+    params.z = z0;
+    params.pGeo = pGeo0;
 
-a_prior = 1;
-b_prior = 1;
-params.a_prior = a_prior;
-params.b_prior = b_prior;
+    a_prior = 1;
+    b_prior = 1;
+    params.a_prior = a_prior;
+    params.b_prior = b_prior;
 
-tau_collect = [];
-like_ests = zeros(1,M);
-pGeo_collect = zeros(1,M);
-corr_mat_collect =  zeros(n,n,M);
-z_collect = zeros(M,T);
-joint_loglik_collect = zeros(1,M);
-rho_record_collect = zeros(M,T);
+    tau_collect = [];
+    like_ests = zeros(1,M);
+    pGeo_collect = zeros(1,M);
+    corr_mat_collect =  zeros(n,n,M);
+    z_collect = zeros(M,T);
+    joint_loglik_collect = zeros(1,M);
+    rho_record_collect = zeros(M,T);
 
-tic;
-for m=1:M
-    disp(m)
+    tic;
+    for m=1:M
+        disp(m)
 
-    % update all params
-    [tau,params,corr_mat,joint_loglik,rho_record] = particleGibbs(N,tau,params);
+        % update all params
+        [tau,params,corr_mat,joint_loglik,rho_record] = particleGibbs(N,tau,params);
 
-    % save results
-    tau_collect = [tau_collect,tau];
-    pGeo_collect(m) = params.pGeo;
-    z_collect(m,:) = params.z;
-    corr_mat_collect(:,:,m) = corr_mat;
-    joint_loglik_collect(m) = joint_loglik;
-    rho_record_collect(m,:) = rho_record;
+        % save results
+        tau_collect = [tau_collect,tau];
+        pGeo_collect(m) = params.pGeo;
+        z_collect(m,:) = params.z;
+        corr_mat_collect(:,:,m) = corr_mat;
+        joint_loglik_collect(m) = joint_loglik;
+        rho_record_collect(m,:) = rho_record;
+    end
+    time_taken = toc;
+
+    save_fname = sprintf('%s/distribution_%s_gdpprior_%s_M_%i.mat',...
+        experiment_dir,distribution_name,string(GDPPrior),M);
+
+    parforsave(save_fname,tau_collect,pGeo_collect,z_collect,...
+                         corr_mat_collect,joint_loglik_collect,rho_record_collect,...
+                         time_taken,M,N,n_quadrature_points,constrain_rho,...
+                         params);
+
+
 end
-time_taken = toc;
-
-save_fname = sprintf('%s/distribution_%s_gdpprior_%s_M_%i.mat',...
-    experiment_dir,distribution_name,string(GDPPrior),M);
-
-parforsave(save_fname,tau_collect,pGeo_collect,z_collect,...
-                     corr_mat_collect,joint_loglik_collect,rho_record_collect,...
-                     time_taken,M,N,n_quadrature_points,constrain_rho,...
-                     params);
-
-
-
 
 
 
